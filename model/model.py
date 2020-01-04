39de2986cf08a094d63e93da770f504fd8680423
szymswiat <szymswiat5@gmail.com>
2020-01-04 19:05:04 +0100

    Fix very slow model compilation time
    - Refactor some code

--- model/model.py
+++ model/model.py
@@ -2,43 +2,42 @@ from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D,
 from keras.models import Model
 
 
-def get_unet(n_ch, img_height, img_width) -> Model:
-    inputs = Input(shape=(n_ch, img_height, img_width))
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(inputs)
+def get_unet(n_classes, n_ch, img_height, img_width) -> Model:
+    inputs = Input(shape=(img_height, img_width, n_ch))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
     conv1 = Dropout(0.2)(conv1)
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D((2, 2))(conv1)
     #
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
     conv2 = Dropout(0.2)(conv2)
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
     pool2 = MaxPooling2D((2, 2))(conv2)
     #
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
     conv3 = Dropout(0.2)(conv3)
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
 
     up1 = UpSampling2D(size=(2, 2))(conv3)
-    up1 = concatenate([conv2, up1], axis=1)
-    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(up1)
+    up1 = concatenate([conv2, up1], axis=3)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1)
     conv4 = Dropout(0.2)(conv4)
-    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv4)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
     #
     up2 = UpSampling2D(size=(2, 2))(conv4)
-    up2 = concatenate([conv1, up2], axis=1)
-    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(up2)
+    up2 = concatenate([conv1, up2], axis=3)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up2)
     conv5 = Dropout(0.2)(conv5)
-    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_first')(conv5)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)
     #
-    conv6 = Conv2D(3, (1, 1), activation='relu', padding='same', data_format='channels_first')(conv5)
-    conv6 = core.Reshape((3, img_height * img_width))(conv6)
-    conv6 = core.Permute((2, 1))(conv6)
+    conv6 = Conv2D(n_classes, (1, 1), activation='relu', padding='same')(conv5)
+    conv6 = core.Reshape((img_height, img_width, n_classes))(conv6)
+    # conv6 = core.Permute((2, 1))(conv6)
     ############
     conv7 = core.Activation('softmax')(conv6)
 
     model = Model(inputs=inputs, outputs=conv7)
 
     # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
-    model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
 
     return model
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import configparser
 import os
 
+from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from keras.utils.vis_utils import plot_model as plot
 
@@ -8,47 +9,94 @@ from keras.utils.vis_utils import plot_model as plot
 from model.model import get_unet
 from training.data_provider import get_batch_gen
 
-config = configparser.RawConfigParser()
-config.read('configuration.ini')
 
-# Training settings
-n_epochs = int(config.get('training', 'n_epochs'))
-# batch_size = int(config.get('training settings', 'batch_size'))
-mode = config.get('training', 'mode')
+class Config:
+    def __init__(self, filepath):
+        parser = configparser.RawConfigParser()
+        parser.read(filepath)
 
-batch_gen = get_batch_gen(
-    imgs_path=config.get('data paths', 'imgs'),
-    gt_masks_path=config.get('data paths', 'gt_masks')
-)
+        self.n_epochs = int(parser.get('training', 'n_epochs'))
 
-# =========== Construct and or load the model architecture
-model = None
+        self.mode = parser.get('training', 'mode')
+        self.batch_size = int(parser.get('training', 'batch_size'))
+        self.model_output_dir = parser.get('training', 'output')
+        self.model_name = parser.get('model', 'name')
 
-model_output_dir = config.get('training', 'output')
-model_name = config.get('model', 'name')
+        self.n_classes = int(parser.get('model', 'n_classes'))
+        self.n_ch = int(parser.get('model', 'img_channels'))
+        self.height = int(parser.get('model', 'img_height'))
+        self.width = int(parser.get('model', 'img_width'))
 
-model_output_dir_path = '{}/{}'.format(model_output_dir, model_name)
-if os.path.isdir(model_output_dir_path) is False:
-    os.mkdir(model_output_dir_path)
+        self.imgs_path_train = parser.get('data paths', 'imgs_train')
+        self.gt_masks_path_train = parser.get('data paths', 'gt_masks_train')
 
-if mode == 'new':
-    n_ch = int(config.get('model', 'img_channels'))
-    height = int(config.get('model', 'img_height'))
-    width = int(config.get('model', 'img_width'))
-    model = get_unet(n_ch, height, width)  # the U-net model
-    print("Check: final output of the network:")
-    print(model.output_shape)
-    plot(model, to_file='{}/{}_model.png'.format(model_output_dir_path, model_name))  # check how the model looks like
-    json_string = model.to_json()
-    open('{}/{}_architecture.json'.format(model_output_dir_path, model_name), 'w').write(json_string)
-elif mode == 'resume':
-    model = load_model('{}/{}_whole_model.h5'.format(model_output_dir_path, model_name))
+        self.imgs_path_val = parser.get('data paths', 'imgs_val')
+        self.gt_masks_path_val = parser.get('data paths', 'gt_masks_val')
 
-# ============  Training
-model.fit_generator(generator=batch_gen, epochs=n_epochs, verbose=2)
+        self.train_steps = int(parser.get('training', 'train_steps'))
+        self.val_steps = int(parser.get('training', 'val_steps'))
 
-# ========== Save model
-model.save_weights('{}/{}_last_weights.h5'.format(model_output_dir_path, model_name), overwrite=True)
-model.save('{}/{}_whole_model.h5'.format(model_output_dir_path, model_name))
 
-print("\n\n========  Finished Training =======================")
+def create_callbacks(weights_dir):
+    callbacks = []
+
+    # checkpoint
+    filepath = '{}/{}'.format(weights_dir, "model-{epoch:02d}-{val_accuracy:.2f}.hdf5")
+    checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
+
+    callbacks.append(checkpoint)
+
+    return callbacks
+
+
+def main():
+    c = Config('configuration.ini')
+
+    train_gen = get_batch_gen(
+        imgs_path=c.imgs_path_train,
+        gt_masks_path=c.gt_masks_path_train
+    )
+    val_gen = get_batch_gen(
+        imgs_path=c.imgs_path_val,
+        gt_masks_path=c.gt_masks_path_val
+    )
+
+    # =========== Construct and or load the model architecture
+    model = None
+
+    model_output_dir_path = '{}/{}'.format(c.model_output_dir, c.model_name)
+    if os.path.isdir(model_output_dir_path) is False:
+        os.mkdir(model_output_dir_path)
+
+    if c.mode == 'new':
+
+        model = get_unet(c.n_classes, c.n_ch, c.height, c.width)  # the U-net model
+        print("Check: final output of the network:")
+        print(model.summary())
+        plot(model,
+             to_file='{}/{}_model.png'.format(model_output_dir_path, c.model_name))  # check how the model looks like
+        json_string = model.to_json()
+        open('{}/{}_architecture.json'.format(model_output_dir_path, c.model_name), 'w').write(json_string)
+
+        model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+    elif c.mode == 'resume':
+        model = load_model('{}/{}_whole_model.h5'.format(model_output_dir_path, c.model_name))
+    else:
+        raise Exception("Invalid mode.")
+
+    callbacks = create_callbacks(model_output_dir_path)
+
+    model.fit_generator(
+        generator=train_gen,
+        steps_per_epoch=c.train_steps,
+        validation_data=val_gen,
+        validation_steps=c.val_steps,
+        epochs=c.n_epochs,
+        callbacks=callbacks,
+        verbose=2)
+
+    print("\n\n======================= Training finished =======================")
+
+
+if __name__ == "__main__":
+    main()
