b43f4a5217ea3188db8c79f1fb8a09ddb3eec356
szymswiat <szymswiat5@gmail.com>
2020-01-16 10:02:08 +0100

    Modify predict.py to display n classes

--- bin/predict.py
+++ bin/predict.py
@@ -24,22 +24,33 @@ def parse_args(args):
 
 
 def postprocess_data(images_batch, predictions, gt_masks_batch):
-    b, h, w, ch = images_batch.shape
+    images_batch = images_batch[0]
+    gt_masks_batch = gt_masks_batch[0]
+    predictions = predictions[0]
+    h, w, ch = images_batch.shape
     gt_classes_map = map_to_classes(gt_masks_batch)
-    gt_mask_to_display = np.zeros(shape=gt_classes_map.shape, dtype=np.uint8)
-    gt_mask_to_display[gt_classes_map == 1] = 255
-    gt_mask_to_display = gt_mask_to_display.reshape((b, h, w))
-
     pred_classes_map = map_to_classes(predictions)
-    pred_mask_to_display = np.zeros(shape=pred_classes_map.shape, dtype=np.uint8)
-    pred_mask_to_display[pred_classes_map == 1] = 255
-    pred_mask_to_display = pred_mask_to_display.reshape((b, h, w))
+
+    gt_masks = []
+    pred_masks = []
+
+    for i in range(predictions.shape[-1]):
+        gt_class_mask = np.zeros(shape=gt_classes_map.shape, dtype=np.uint8)
+        gt_class_mask[gt_classes_map == i] = 255
+        gt_class_mask = gt_class_mask.reshape((h, w))
+
+        pred_class_mask = np.zeros(shape=pred_classes_map.shape, dtype=np.uint8)
+        pred_class_mask[pred_classes_map == i] = 255
+        pred_class_mask = pred_class_mask.reshape((h, w))
+
+        gt_masks.append(gt_class_mask)
+        pred_masks.append(pred_class_mask)
 
     images_batch += 1.
     images_batch *= 127.5
-    image = images_batch[0].astype(dtype=np.uint8)
+    image = images_batch.astype(dtype=np.uint8)
 
-    return image, pred_mask_to_display[0], gt_mask_to_display[0]
+    return image, gt_masks, pred_masks
 
 
 def main(args=None):
@@ -63,19 +74,21 @@ def main(args=None):
 
         predictions = predictions.numpy()
 
-        image, prediction, gt = postprocess_data(images_batch, predictions, gt_masks_batch)
+        image, gt_masks, pred_masks = postprocess_data(images_batch, predictions, gt_masks_batch)
 
         fig = plt.figure(
             dpi=200
         )
-        gs = fig.add_gridspec(1, 3)
+        gs = fig.add_gridspec(3, len(pred_masks))
 
-        fig.add_subplot(gs[0, 0])
-        plt.imshow(image)
         fig.add_subplot(gs[0, 1])
-        plt.imshow(prediction, cmap='gray')
-        fig.add_subplot(gs[0, 2])
-        plt.imshow(gt, cmap='gray')
+        plt.imshow(image)
+
+        for i in range(len(pred_masks)):
+            fig.add_subplot(gs[1, i])
+            plt.imshow(pred_masks[i], cmap='gray')
+            fig.add_subplot(gs[2, i])
+            plt.imshow(gt_masks[i], cmap='gray')
 
         plt.show()
 
